99e74305749ca295e947da2a037ce6b4242b3e08
trukozak <aleksandr.nepijvoda@gmail.com>
2021-06-15 23:52:19 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,23 +1,42 @@
-const refs = {
-  input: document.querySelector('input'),
-  create: document.querySelector('[data-action="render"]'),
-  destroy: document.querySelector('[data-action="destroy"]')
-}
-console.log(refs.input);
+const inputRef = document.querySelector('[type="number"]');
+const addButtonRef = document.querySelector('[data-action="render"]');
+const removeButtonRef = document.querySelector('[data-action="destroy"]');
+const boxRef = document.querySelector('#boxes');
 
-refs.input.addEventListener('input', onValueBoxes)
-refs.create.addEventListener('click', onCreateBoxes)
+const rgbMaxValue = 255;
+const getRandomNum = () => Math.floor(Math.random() * rgbMaxValue);
+const getRandomColor = () =>
+  `rgb(${getRandomNum()}, ${getRandomNum()}, ${getRandomNum()})`;
 
-function onValueBoxes() {
-  const resValue = refs.input.value
-  console.log(resValue);
-}
-function onCreateBoxes(amount) {
-  amount.forEach(element => {
-    const res = document.createElement('div')
-    element = res
-    console.log(element);
+const primaryValue = 30;
+const step = 10;
 
-  });
-}
-onCreateBoxes([2])
+const createBoxes = amount => {
+  const nextBox = boxRef.lastChild;
+  const initialBoxSize =
+    nextBox === null
+      ? primaryValue - step
+      : +nextBox.style.width.replace('px', '');
+
+  const divAll = [];
+  for (let i = 1; i <= amount; i += 1) {
+    const div = document.createElement('div');
+    const size = `${initialBoxSize + step * i}px`;
+    div.style.width = size;
+    div.style.height = size;
+    div.style.backgroundColor = getRandomColor();
+    divAll.push(div);
+  }
+  boxRef.append(...divAll);
+};
+
+
+addButtonRef.addEventListener('click', onBtnRenderClick => {
+  const inputValue = inputRef.value;
+  createBoxes(inputValue);
+});
+
+removeButtonRef.addEventListener(
+  'click',
+  destroyBoxes => (boxRef.innerHTML = ''),
+);
